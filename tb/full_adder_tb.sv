e75b67c644decbc0d44a9a92c7094e16afeae905
Kiana Jafari <kiana.jafari.ai@gmail.com>
2026-05-25 09:59:08 +0330

    Refactor display format in full adder testbench

--- tb/full_adder_tb.sv
+++ tb/full_adder_tb.sv
@@ -2,12 +2,12 @@ module full_adder_tb;
 
 logic A;
 logic B;
-
 logic Cin;
+
 logic Sum;
 logic Cout;
 
-// Design Under Test (DUT)
+// Design Under Test
 full_adder dut(
     .A(A),
     .B(B),
@@ -20,21 +20,45 @@ initial begin
     $display("A | B | Cin | Cout | Sum");
     $display("------------------------");
 
+    A = 0; B = 0; Cin = 0;
+    #10;
+    $display("%b | %b |  %b  |  %b   |  %b",
+             A, B, Cin, Cout, Sum);
+
     A = 0; B = 0; Cin = 1;
     #10;
-    $display("%b | %b | %b | %b | %b", A, B, Cin, Cout, Sum);
+    $display("%b | %b |  %b  |  %b   |  %b",
+             A, B, Cin, Cout, Sum);
+
+    A = 0; B = 1; Cin = 0;
+    #10;
+    $display("%b | %b |  %b  |  %b   |  %b",
+             A, B, Cin, Cout, Sum);
 
     A = 0; B = 1; Cin = 1;
     #10;
-    $display("%b | %b | %b | %b | %b", A, B, Cin, Cout, Sum);
+    $display("%b | %b |  %b  |  %b   |  %b",
+             A, B, Cin, Cout, Sum);
+
+    A = 1; B = 0; Cin = 0;
+    #10;
+    $display("%b | %b |  %b  |  %b   |  %b",
+             A, B, Cin, Cout, Sum);
 
     A = 1; B = 0; Cin = 1;
     #10;
-    $display("%b | %b | %b | %b | %b", A, B, Cin, Cout, Sum);
+    $display("%b | %b |  %b  |  %b   |  %b",
+             A, B, Cin, Cout, Sum);
+
+    A = 1; B = 1; Cin = 0;
+    #10;
+    $display("%b | %b |  %b  |  %b   |  %b",
+             A, B, Cin, Cout, Sum);
 
     A = 1; B = 1; Cin = 1;
     #10;
-    $display("%b | %b | %b | %b | %b", A, B, Cin, Cout, Sum);
+    $display("%b | %b |  %b  |  %b   |  %b",
+             A, B, Cin, Cout, Sum);
 
     $finish;
 end
